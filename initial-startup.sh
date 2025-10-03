a26b563051f6fd746b1202ff1e4692ac57055271
Darren <darren.moreland@icloud.com>
2025-10-03 14:47:48 -0400

    Updated README files

--- initial-startup.sh
+++ initial-startup.sh
@@ -3,3 +3,6 @@
 docker-compose up -d --force-recreate --build
 sleep 10
 docker exec -it clinical-database python3 db-import.py
+python -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
